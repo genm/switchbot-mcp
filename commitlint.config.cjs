c256fbf0a20f092b166e6b07279e869cb77f49bc
genm <genm@users.noreply.github.com>
2026-02-21 15:41:43 +0800

    chore: enforce ASCII-only English commit messages in commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "is-ascii": ({ header, body, footer }) => {
+          const isAscii = (str) => !str || /^[\x00-\x7F]*$/.test(str);
+          return [
+            isAscii(header) && isAscii(body) && isAscii(footer),
+            "Commit message must be in English (ASCII only)",
+          ];
+        },
+      },
+    },
+  ],
+  rules: {
+    "is-ascii": [2, "always"],
+    "header-max-length": [2, "always", 72],
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+  },
 };
